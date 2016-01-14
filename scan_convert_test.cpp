581138755cc21f5c0a1343054f843995b1e24009
Joshua Loew <loewjc@gmail.com>
2016-01-14 12:18:13 -0500

    Added a much more comprehensive test suite

--- scan_convert_test.cpp
+++ scan_convert_test.cpp
@@ -1,65 +1,39 @@
 #include "glsupport.h"
 #include "screen_buffer.h"
 #include "my_scan_converter.h"
+#include "math.h"
+#include "stdio.h"
 
-static void initScene(ScreenBuffer * buf)
-{
-    MyScanConverter msc;
-
-    // horizontal line forwards
-    // msc.drawLine(buf, 30, 30, 50, 30);
-
-    // horizontal line backwards
-    // msc.drawLine(buf, 50, 30, 30, 30);
-
-    // vertical line forwards
-    // msc.drawLine(buf, 30, 20, 30, 60);
-
-    // vertical line backwards
-    // msc.drawLine(buf, 30, 60, 30, 20);
-
-    // shallow positive slope
-    // msc.drawLine(buf, 10, 10, 40, 30);
-
-    // reversed shallow positive slope
-    // msc.drawLine(buf, 40, 30, 10, 10);
-
-    // deep positive slope
-    // msc.drawLine(buf, 10, 10, 20, 40);
-
-    // reversed deep positive slope
-    // msc.drawLine(buf, 20, 40, 10, 10);
-
-    // shallow negative slope
-    msc.drawLine(buf, 0, 30, 30, 20);
-
-    // reversed shallow negative slope
-
-    // origin to third quadrant
-
-    // third quadrant to origin
-
-    // origin to fourth quadrant
-
-    // fourth quadrant to origin
-
-    // msc.drawLine(buf, 50, 50, 50, 10);
-    // msc.drawLine(buf, 10, 10, 50, 25);
-    //
-    // buf -> energizePixel(50, 25, 0.5);
-    // buf -> energizePixel(50, 50, 0.5);
-    // buf -> energizePixel(50, 10, 0.5);
+static void testScene(ScreenBuffer * buf) {
+    MyScanConverter m;
+    for (float angle = 0; angle < M_PI * 2; angle += M_PI/32) {
+        float a_y = sin(angle) * 150;
+        float a_x = cos(angle) * 150;
+        m.drawLine(buf, 200, 200, 200 + (int) a_x, 200 + (int) a_y);
+    }
 }
 
-int main(int argc, char **argv)
-{
+static void initScene(ScreenBuffer * buf) {
+    MyScanConverter msc;
+    msc.drawLine(buf, 10, 10, 50, 10);
+    msc.drawLine(buf, 10, 10, 50, 50);
+    msc.drawLine(buf, 50, 50, 50, 10);
+    cout << "diagonal" << "\n";
+    msc.drawLine(buf, 10, 10, 50, 25);
+
+    buf -> energizePixel(50, 25, 0.5);
+    buf -> energizePixel(50, 50, 0.5);
+    buf -> energizePixel(50, 10, 0.5);
+}
+
+int main(int argc, char **argv) {
     try {
         glutInit(&argc, argv);
 
-        int pixel_width = 8;
-        int pixel_height = 8;
-        int buffer_width = 64;
-        int buffer_height = 64;
+        int pixel_width = 2;
+        int pixel_height = 2;
+        int buffer_width = 400;
+        int buffer_height = 400;
 
         cout << "screen buffer being constructed" << "\n";
 
@@ -69,7 +43,8 @@ int main(int argc, char **argv)
 
         glewInit();
 
-        initScene(buf);
+        //initScene(buf);
+        testScene(buf);
 
         glutMainLoop();
         return 0;
